9976e2c23fdace822ef584cdeb58b6c74245deef
Chrisjoi503 <102913064+Chrisjoi503@users.noreply.github.com>
2023-06-07 22:17:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ function handleCredentialResponse(response) {
     const credential = response.credential;
     const id_token = credential.id_token;
   
-    // Use the id_token or credential for further processing
-    // e.g., send it to a server for authentication
-  
-    // For this example, we'll simply display the user's ID and name
     const profile = credential.profile;
     const userId = profile.id;
     const userName = profile.name;
